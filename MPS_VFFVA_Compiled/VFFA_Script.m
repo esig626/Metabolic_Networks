bc74eab0a2de3506c507c610cf064b16cce7ef8f
Elias Vera-Siguenza <132355398+esig626@users.noreply.github.com>
2023-05-03 17:58:50 +0100

    Add files via upload

---
+++ MPS_VFFVA_Compiled/VFFA_Script.m
@@ -0,0 +1,40 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% VFFVA TEST!
+% Load the model you need
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%
+nCores=3;
+nThreads=2;
+ex='';
+%%
+path = '/Users/elias/Documents/MATLAB/Pipeline_mCadre/Clean_Version/mCADRE_New/MPS';
+cd(path)
+modelmps = model;
+output_filename = 'model';
+convertProblem(modelmps, 0, output_filename);
+modelmps = 'model.mps';
+cd ..
+
+%%
+
+disp('Hold on to your seat, I am initialising the system ...')
+%Set install folder of MPI
+setenv('PATH', ['/opt/local/bin:', getenv('PATH')]);
+[status, cmdout] = system('mpiexec --version');
+%Set schedule and chunk size parameters
+setenv('OMP_SCHEDUELE', ['dynamic' ',' num2str(50)])
+% Get the .mps formatted model 
+cd(path); 
+clc
+% Run VFFVA!
+disp('Running Very Fast Flux Variability Analysis!')
+[status,cmdout]=system(['mpirun -np ' num2str(nCores)...
+' --bind-to ' num2str('none') ' -x OMP_NUM_THREADS=' num2str(nThreads)...
+' ./veryfastFVA ' modelmps ' ' num2str(90)  ' ' num2str(0) ' ' ex]);
+clc
+disp('I am done! I will now fetch your results, hang tight...')
+resultFile=[modelmps(1:end-4) 'output.csv'];
+results=readtable(resultFile);
+opt=[results.minFlux,results.maxFlux];
+
+clearvars -except opt model
